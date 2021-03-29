5ea21c3c49d77d34a7cf73a87537a0eba424625d
choogland <christine.hoogland@gmail.com>
2021-03-29 12:14:31 +0200

    update action

--- download.R
+++ download.R
@@ -8,6 +8,7 @@ today <- format(Sys.time(), "%Y-%m-%d")
 tests_data_today <- tests_data %>%
   filter(date == today)
 
-readr::write_csv(tests_data_today, paste0("data/tests_",today,".csv"))
+#readr::write_csv(tests_data_today, paste0("data/tests_",today,".csv"))
+readr::write_csv(tests_data_today, "data/tests_today.csv")
 
                  
