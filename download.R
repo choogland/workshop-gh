002e8d7d63d35d10757221efcaea31c16c645fb8
choogland <christine.hoogland@gmail.com>
2021-03-29 11:57:44 +0200

    adding first task

---
+++ download.R
@@ -0,0 +1,13 @@
+library(dplyr)
+library(readr)
+
+tests_data <- readr::read_csv(
+  "https://raw.githubusercontent.com/dsbbfinddx/FINDCov19TrackerData/master/processed/coronavirus_tests.csv")
+
+today <- format(Sys.time(), "%Y-%m-%d")
+tests_data_today <- tests_data %>%
+  filter(date == today)
+
+readr::write_csv(tests_data_today, paste0("data/tests_",today,".csv"))
+
+                 
